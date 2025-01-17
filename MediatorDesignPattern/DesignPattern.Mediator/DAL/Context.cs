c9c248f5af2bdd2955d5a70b000702dc35ac98a3
Aytuu1 <Aytuuc1@gmail.com>
2025-01-18 01:01:16 +0300

    Mediator Design Pattern

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Mediator.DAL
+{
+  public class Context : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer("Server=DESKTOP-EGM53VS\\SQLAYTU; initial Catalog=MediatRDesignPattern; integrated security=true; TrustServerCertificate=True;");
+    }
+    public DbSet<Product> Products { get; set; }
+  }
+}
---
+++ MediatorDesignPattern/DesignPattern.Mediator/MediatorPattern/Queries/GetAllProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using DesignPattern.Mediator.MediatorPattern.Results;
+using MediatR;
+
+namespace DesignPattern.Composite.MediatorPattern.Queries
+{
+  public class GetAllProductQuery : IRequest<List<GetAllProductQueryResult>>
+  {
+
+  }
+}
